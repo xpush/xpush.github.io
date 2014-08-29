3a4094b963b47e27bb8458f3b1bd81d2fb1ea339
EunsanKo <eunsan.ko@gmail.com>
2014-08-29 09:49:26 +0900

    ^^

--- assets/js/magister.js
+++ assets/js/magister.js
@@ -1,4 +1,4 @@
-// global. currently active menu item 
+// global. currently active menu item
 var current_item = 0;
 
 // few settings
@@ -9,18 +9,19 @@ var section_show_time = 1300;
 jQuery(document).ready(function($) {
 
 	// Switch section
-	$("a", '.mainmenu').click(function() 
+	$("a", '.mainmenu').click(function()
 	{
-		if( ! $(this).hasClass('active') ) { 
+			
+		if( ! $(this).hasClass('active') ) {
 			current_item = this;
 			// close all visible divs with the class of .section
-			$('.section:visible').fadeOut( section_hide_time, function() { 
-				$('a', '.mainmenu').removeClass( 'active' );  
+			$('.section:visible').fadeOut( section_hide_time, function() {
+				$('a', '.mainmenu').removeClass( 'active' );
 				$(current_item).addClass( 'active' );
 				var new_section = $( $(current_item).attr('href') );
 				new_section.fadeIn( section_show_time );
 			} );
 		}
 		return false;
-	});		
+	});
 });
